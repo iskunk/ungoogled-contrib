401c55eefde086be78e2fc5f2a44f72dddcdb634
Daniel Richard G <skunk@iSKUNK.ORG>
2023-06-13 01:33:25 -0400

    Draft of new Debian Chromium package conversion

---
+++ debian-convert/editcontrol.pl
@@ -0,0 +1,88 @@
+#!/usr/bin/env perl
+# editcontrol.pl
+#
+# Script to simplify automated editing of Debian control files
+# (see deb-src-control(5) for information on the format)
+#
+# Usage samples:
+#
+#   editcontrol -e '$package =~ /foo/ and s/foo/bar/' control >control.new
+#
+#   editcontrol -i -e 's/foo/bar/;' -e 's/bar/qux/' debian/control
+#
+#   editcontrol -i=.orig -f script.pl debian/control
+#
+
+use strict;
+use warnings;
+
+use Dpkg::Control::Info;
+use Getopt::Long;
+
+my @expr_list = ();
+my $script_file;
+my $inplace;
+
+sub load_script_file
+{
+	my ($opt_name, $opt_value) = @_;
+	open(F, $opt_value) or die "$opt_value: $!";
+	local $/;
+	my $script = <F>;
+	close(F);
+	push @expr_list, $script;
+}
+
+GetOptions(
+	'expr=s' => \@expr_list,
+	'file=s' => \&load_script_file,
+	'inplace:s' => \$inplace
+) or exit 1;
+
+push @expr_list, ';1;';
+my $expr = join("\n", @expr_list);
+my $input_file = $ARGV[0];
+
+my $control_info = Dpkg::Control::Info->new(filename => $input_file);
+
+foreach our $control (@{$control_info})
+{
+	my $source  = $control->{'Source'}  || '';
+	my $package = $control->{'Package'} || '';
+
+	# Subroutine callable from the user expression
+	#
+	sub set_field($$) {
+		my ($name, $value) = @_;
+		$control->{$name} = $value;
+	}
+
+	foreach my $field (keys(%{$control}))
+	{
+		local $_ = $control->{$field};
+
+		eval $expr or die $@;
+
+		if (defined($_)) { $control->{$field} = $_; }
+		else { delete $control->{$field}; }
+	}
+}
+
+my $out = $control_info->output() or die;
+
+if (defined($inplace))
+{
+	local @ARGV = $input_file;
+	local $^I = $inplace;
+	local $/;
+	my $orig = <>;
+	print($out);
+}
+else
+{
+	print($out);
+}
+
+exit 0;
+
+# end editcontrol.pl
